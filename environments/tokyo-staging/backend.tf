4416c26322ce8a442abbf9d0242d61d5d0e4c59c
PhamThanh <robertthanh1107@gmail.com>
2026-03-03 10:35:19 +0700

    feat: update backend s3 without dynamodb

--- environments/tokyo-staging/backend.tf
+++ environments/tokyo-staging/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "welfan-namecard-tfstates"
-    region         = "ap-northeast-1"
-    key            = "tokyo-staging/terraform.tfstate"
-    dynamodb_table = "welfan-namecard-tokyo-staging-tfstates-locking"
-    profile        = "liongarden-mfa"
+    bucket       = "care-hub-tfstate-storage-aet"
+    region       = "ap-northeast-1"
+    key          = "tokyo-dev/terraform.tfstate"
+    profile      = "clinicmanagementaccount-mfa"
+    use_lockfile = true
   }
 }
